4ed68ee4fe3a2feb15e4525ca41e49c314e4a5f1
Gustavo Ridgue <guferrari007@gmail.com>
2020-01-24 10:41:25 -0300

    Exercicios de lógica S3_A4

---
+++ src/Sistema/Teste2.java
@@ -0,0 +1,20 @@
+package Sistema;
+
+public class Teste2 {
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30};
+        int i = 0;
+
+        for(int b : a){
+            i += b;
+            b++;
+
+            if(i % 2 == 0){
+                i--;
+            }
+        }
+
+        System.out.println(i);
+    }
+}
+
